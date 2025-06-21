f91278b07c272baa039c76a16e183282c06724d9
Sishir Shrestha <sishershrestha000@gmail.com>
2025-06-21 15:03:03 +0545

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,26 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+// app/layout.tsx
+'use client';
+
+import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          <TooltipProvider>
+            <Toaster />
+            <Sonner />
+            {children}
+          </TooltipProvider>
+        </QueryClientProvider>
+      </body>
     </html>
-  )
+  );
 }
